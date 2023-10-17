75099195373be0ad0545d07d1b395684943a1d4a
igrzi <igorzimmer01@gmail.com>
2023-10-17 11:38:50 -0300

    feat: Implemented recursive fibonacci function

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+func recursiveFib(n int) int {
+	if n == 0 {
+		return 0
+	} else if n <= 2 {
+		return 1
+	} else if n > 40 {
+		return -1
+	}
+	return recursiveFib(n-1) + recursiveFib(n-2)
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	var fibNumber int
+	fmt.Print("Type the number of Fibonacci you want: ")
+	fmt.Scan(&fibNumber)
+
+	fmt.Printf("\n[recursiveFib] | fib(%v): Value: %v", fibNumber, recursiveFib(fibNumber))
 }
