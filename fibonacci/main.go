33e5fa80a9091bd6f60bb9e00ef1ff71856104ec
igrzi <igorzimmer01@gmail.com>
2023-10-17 12:16:40 -0300

    feat: Implemented input validation for fibNumber

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 )
 
 var memo = make(map[int]int)
+
+func getFibonacciInput() (int, error) {
+	var fibNumberInput string
+
+	fmt.Print("Type the number of Fibonacci you want: ")
+	fmt.Scan(&fibNumberInput)
+
+	fibNumber, err := strconv.Atoi(fibNumberInput)
+	if err != nil {
+		return 0, errors.New("please enter a valid integer")
+	}
+
+	if fibNumber < 0 {
+		return 0, errors.New("please enter a positive integer")
+	}
+
+	return fibNumber, nil
+}
 
 func recursiveFib(n int) int {
 	if n == 0 {
@@ -29,9 +49,11 @@ func linearFib(n int) int {
 }
 
 func main() {
-	var fibNumber int
-	fmt.Print("Type the number of Fibonacci you want: ")
-	fmt.Scan(&fibNumber)
+	fibNumber, err := getFibonacciInput()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
 	fmt.Printf("\n[recursiveFib] | fib(%v): Value: %v", fibNumber, recursiveFib(fibNumber))
 	fmt.Printf("\n[linearFib]    | fib(%v): Value: %v", fibNumber, linearFib(fibNumber))
