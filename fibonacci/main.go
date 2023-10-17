1e9e0759bf81bb7ffbed6e1218a388f255eab3bc
igrzi <igorzimmer01@gmail.com>
2023-10-17 11:56:20 -0300

    feat: Implemented linearFib using memoization

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -4,15 +4,28 @@ import (
 	"fmt"
 )
 
+var memo = make(map[int]int)
+
 func recursiveFib(n int) int {
 	if n == 0 {
 		return 0
 	} else if n <= 2 {
 		return 1
-	} else if n > 40 {
+	} else if n >= 50 {
 		return -1
 	}
 	return recursiveFib(n-1) + recursiveFib(n-2)
+}
+
+func linearFib(n int) int {
+	if memo[n] != 0 {
+		return memo[n]
+	} else if n <= 2 {
+		return 1
+	}
+
+	memo[n] = (linearFib(n-1) + linearFib(n-2))
+	return memo[n]
 }
 
 func main() {
@@ -21,4 +34,5 @@ func main() {
 	fmt.Scan(&fibNumber)
 
 	fmt.Printf("\n[recursiveFib] | fib(%v): Value: %v", fibNumber, recursiveFib(fibNumber))
+	fmt.Printf("\n[linearFib]    | fib(%v): Value: %v", fibNumber, linearFib(fibNumber))
 }
